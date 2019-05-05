3ef6284c4b4f310aaceb351bee6a96eae553b21e
inove0412 <812500980@qq.com>
2019-05-05 16:29:33 +0800

    mall商城源码

---
+++ src/views/pms/product/save.vue
@@ -0,0 +1,12 @@
+<template> 
+  <product-detail :is-edit='true'></product-detail>
+</template>
+<script>
+  import ProductGetDetail from './components/ProductGetDetail'
+  export default {
+    name: 'getUpdateInfo',
+    components: { ProductGetDetail }
+  }
+</script>
+<style>
+</style>
